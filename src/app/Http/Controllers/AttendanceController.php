329a37252cca20f3225218a7aa78197d52141ab0
Alson Loquinario <alson.loquinario@antiquespride.edu.ph>
2024-11-15 12:19:37 +0800

    rebuild project

--- src/app/Http/Controllers/AttendanceController.php
+++ src/app/Http/Controllers/AttendanceController.php
@@ -103,6 +103,17 @@ class AttendanceController extends Controller
                     // If am_time_in is not null and current time is past 12 PM but before 1 PM, store time in am_time_out
                     $attendance->am_time_out = $formattedTime;
                     $attendance->status = 0;
+                } 
+                if (!is_null($attendance->am_time_in) && $currentHour >= 12 && $currentHour < 13 && $attendance->am_time_out->diffInMinutes($currentTime) >= 15) {
+                    // If am_time_in is not null and current time is past 12 PM but before 1 PM, store time in am_time_out
+                    $attendance->am_time_out = $formattedTime;
+                    $attendance->status = 0;
+                }
+                
+                elseif (!is_null($attendance->am_time_out) && $attendance->am_time_out->diffInMinutes($currentTime) >= 15) {
+                    // If am_time_out is not null and there is a 15-minute interval, store time in pm_time_in
+                    $attendance->pm_time_in = $formattedTime;
+                    $attendance->status = 1;
                 } elseif (is_null($attendance->am_time_out) && $currentHour >= 13) {
                     // If current time is past 1 PM and am_time_out is still null, store time in pm_time_in
                     $attendance->pm_time_in = $formattedTime;
@@ -113,12 +124,12 @@ class AttendanceController extends Controller
                 $attendance->status = 0;
             }
         }
-
+    
         // Automatically set pm_time_in to current time if am_time_out is null and the current time is past 1 PM
         if (is_null($attendance->am_time_out) && $currentHour >= 13 && is_null($attendance->pm_time_in)) {
             $attendance->pm_time_in = $formattedTime;
         }
-
+    
         // Automatically set pm_time_out to 5 PM if it is missing and the current time is past 5 PM
         if (is_null($attendance->pm_time_out) && $currentHour >= 17) {
             $attendance->pm_time_out = Carbon::createFromTime(17, 0, 0)->format('h:i:s A');
